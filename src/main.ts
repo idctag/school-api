a915ba6718947c94841c1cfd262caabc178170e7
idctag <idctag@Battulgas-MacBook-Air.local>
2024-03-22 21:22:41 +0900

    todo comments

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// TODO: implement orm
+// TODO: data base schema
+// TODO: user crud
+// TODO: seed data (faker?)
+// TODO: passport-jwt
+// TODO: argon2 password hash
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
