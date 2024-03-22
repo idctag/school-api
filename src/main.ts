1bf091777cfecf806c213cd6c4701ca4ce38385f
idctag <idctag@Battulgas-MacBook-Air.local>
2024-03-23 02:21:55 +0900

    TODO update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-// TODO: user crud
 // TODO: passport-jwt
+
+// TODO: admin crud
+// NOTE: Role & Jwt guard
+
+// TODO: manager crud
+// NOTE: Role & Jwt guard
+
+// TODO: teacher crud
+// NOTE: Role & Jwt guard
+
+// TODO: student crud
+// NOTE: Role & Jwt guard
+
+// TODO: student crud
+// NOTE: Role & Jwt guard
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
